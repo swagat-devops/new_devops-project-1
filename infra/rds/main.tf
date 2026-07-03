39eb9120878857033ea56f3bb6ee9b48e19ce80a
swagat-devops <swagatdevops@gmail.com>
2026-07-03 16:59:11 +0530

    jenkins

--- infra/rds/main.tf
+++ infra/rds/main.tf
@@ -13,11 +13,11 @@ resource "aws_db_subnet_group" "dev_proje_1_db_subnet_group" {
 }
 
 resource "aws_db_instance" "default" {
-  allocated_storage       = 10
-  storage_type            = "gp2"
-  engine                  = "mysql"
-  engine_version          = "5.7"
-  instance_class          = "db.t2.micro"
+  allocated_storage      = 10
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t4g.micro"
   identifier              = var.mysql_db_identifier
   username                = var.mysql_username
   password                = var.mysql_password
