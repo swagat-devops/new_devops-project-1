0ecd85971e1c1513505bffff4b52417dde3da34f
swagat-devops <swagatdevops@gmail.com>
2026-07-03 16:35:19 +0530

    jenkins

--- infra/variables.tf
+++ infra/variables.tf
@@ -53,7 +53,3 @@ variable "ec2_user_data_install_apache" {
   description = "Script for installing the Apache2"
 }
 
-variable "domain_name" {
-  type = string
-  description = "Name of the domain"
-}
