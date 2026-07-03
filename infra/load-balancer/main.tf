8f2071016e7ee22e831b70313355a2ef15a008e6
swagat-devops <swagatdevops@gmail.com>
2026-07-03 16:10:55 +0530

    change2

--- infra/load-balancer/main.tf
+++ infra/load-balancer/main.tf
@@ -9,9 +9,6 @@ variable "ec2_instance_id" {}
 variable "lb_listner_port" {}
 variable "lb_listner_protocol" {}
 variable "lb_listner_default_action" {}
-variable "lb_https_listner_port" {}
-variable "lb_https_listner_protocol" {}
-variable "dev_proj_1_acm_arn" {}
 variable "lb_target_group_attachment_port" {}
 
 output "aws_lb_dns_name" {
@@ -54,16 +51,3 @@ resource "aws_lb_listener" "dev_proj_1_lb_listner" {
   }
 }
 
-# https listner on port 443
-resource "aws_lb_listener" "dev_proj_1_lb_https_listner" {
-  load_balancer_arn = aws_lb.dev_proj_1_lb.arn
-  port              = var.lb_https_listner_port
-  protocol          = var.lb_https_listner_protocol
-  ssl_policy        = "ELBSecurityPolicy-FS-1-2-Res-2019-08"
-  certificate_arn   = var.dev_proj_1_acm_arn
-
-  default_action {
-    type             = var.lb_listner_default_action
-    target_group_arn = var.lb_target_group_arn
-  }
-}
